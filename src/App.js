b11330517649e8fe55206ae3ce17c3e39ec08a3a
Toxiusus <icek1lla2323@gmail.com>
2024-02-16 16:31:42 +0300

    added routes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import React from 'react';
-import './scss/app.scss';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import Header from './components/Header';
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import { Route } from 'react-router-dom';
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
-
-let fetched = false;
+import "./scss/app.scss";
 
 function App() {
+  const pathname = window.location.pathname;
 
+  
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
           <Routes>
-
+            <Route path="/" element={<Home/>} />
           </Routes>
         </div>
       </div>
