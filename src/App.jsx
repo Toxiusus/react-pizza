7e4d3794183611163c2d43ddfa4c26fb54d071fc
Toxiusus <icek1lla2323@gmail.com>
2024-05-19 19:49:09 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import './scss/app.scss';
 import Header from './components/Header';
 import Home from './pages/Home';
@@ -6,10 +7,9 @@ import NotFound from './pages/NotFound';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  const [searchValue, setSearchValue] = React.useState('');
   return (
     <div className="wrapper">
-      <Header searchValue={searchValue} setSearchValue={setSearchValue} />
+      <Header/>
       <div className="content">
         <Routes>
           <Route path="/" element={<Home />} />
