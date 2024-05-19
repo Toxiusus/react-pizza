e908c56ee6606c8efff4b5340ad0676026ea3a46
Toxiusus <icek1lla2323@gmail.com>
2024-05-19 20:09:19 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
 import './scss/app.scss';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 import NotFound from './pages/NotFound';
-import { Route, Routes } from 'react-router-dom';
 
 function App() {
+  const [searchValue, setSearchValue] = React.useState('');
+
   return (
     <div className="wrapper">
-      <Header/>
+      <Header searchValue={searchValue} setSearchValue={setSearchValue} />
       <div className="content">
         <Routes>
           <Route path="/" element={<Home />} />
--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 import styles from './Search.module.scss'
 import search from '../../assets/img/search.svg'
 
-const Search = () => {
+const Search = ({ searchValue, setSearchValue }) => {
   return (
     <div className={styles.root}>
       <img className={styles.icon} src={search} alt="search" />
-      <input className={styles.input} type="text" placeholder='Поиск пиццы...'/>
+      <input value={searchValue} onChange={(event) => setSearchValue(event.target.value)} className={styles.input} type="text" placeholder='Поиск пиццы...'/>
     </div>
   )
 }
