d842b7bcdec72b5d7a86faf4bb098cad2cd1169e
Toxiusus <icek1lla2323@gmail.com>
2024-05-19 20:20:25 +0300

    fix

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 import styles from './Search.module.scss'
 import search from '../../assets/img/search.svg'
+import close from '../../assets/img/close.svg'
 
 const Search = ({ searchValue, setSearchValue }) => {
   return (
     <div className={styles.root}>
       <img className={styles.icon} src={search} alt="search" />
       <input value={searchValue} onChange={(event) => setSearchValue(event.target.value)} className={styles.input} type="text" placeholder='Поиск пиццы...'/>
+      <img className={styles.close} src={close} alt="close" />
     </div>
   )
 }
