599613053f1269f965bb610582e02a86f4e62ceb
Toxiusus <icek1lla2323@gmail.com>
2024-05-21 21:26:25 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -7,8 +7,11 @@ import App from './App';
 import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
 );
