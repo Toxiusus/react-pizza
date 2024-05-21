7b8633045972659af06c81560bff6c374517c3b1
Toxiusus <icek1lla2323@gmail.com>
2024-05-21 17:55:36 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+import { store } from './redux/store';
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <BrowserRouter>
