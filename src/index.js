8bdf3163373aa41acf2f0b1f7b53ea32a05b9f70
Toxiusus <icek1lla2323@gmail.com>
2024-05-21 21:07:18 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import App from './App';
 
 import { store } from './redux/store';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <App />
