5296a463877d4c5e5a896e34c2d44822688d4802
Toxiusus <icek1lla2323@gmail.com>
2024-05-24 18:54:42 +0300

    fix

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -20,7 +20,15 @@ const cartSlice = createSlice({
 
       if (findItem) {
         findItem.count++;
+      } else {
+        state.items.push({
+          ...action.payload,
+          count: 1,
+        });
       }
+      state.totalPrice = state.items.reduce((sum, obj) => {
+        return (obj.price * obj.count) + sum;
+      }, 0);
     },
     removeItem(state, action) {
       state.items = state.items.filter((obj) => obj.id !== action.payload);
